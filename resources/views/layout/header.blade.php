587fd31cf28277844a77e0c0a37dae3aaccb2e03
Shadab <mohd.shadab@corrintech.com>
2025-09-24 18:57:04 +0530

    shadabuploadnew

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -61,6 +61,18 @@
                         </a>
                     </div>
                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true"
+                        aria-expanded="false">
+                        <i class="fas fa-language"></i> Language
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a href="#" class="dropdown-item change-lang" data-lang="en">English</a>
+                        <a href="#" class="dropdown-item change-lang" data-lang="hi">Hindi</a>
+                        <a href="#" class="dropdown-item change-lang" data-lang="ur">Urdu</a>
+                    </div>
+                </li>
+
 
 
                 <!-- Messages Dropdown Menu -->
