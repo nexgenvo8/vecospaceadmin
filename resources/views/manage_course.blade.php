ff26bb6c788f661cbeb80e253b9a2ce26ecb96b3
Shadab <mohd.shadab@corrintech.com>
2025-10-14 12:42:23 +0530

    shadab

--- resources/views/manage_course.blade.php
+++ resources/views/manage_course.blade.php
@@ -14,6 +14,7 @@
     <title>Course-JMIvecospace</title>
     @include('layout.favicon')
     <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -190,7 +191,8 @@
                                 </div>
 
                                 <!-- Edit Modal -->
-                                <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal fade" id="editModal" tabindex="-1" role="dialog"
+                                    aria-hidden="true">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
 
@@ -343,6 +345,8 @@
             });
         });
     </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- resources/views/manage_notice.blade.php
+++ resources/views/manage_notice.blade.php
@@ -317,6 +317,8 @@
             });
         });
     </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
